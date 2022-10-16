4fb4bb0df4e19aabd6d98de02c65a3b4c9003cb5
GregoryQuerrienPro <gregoryquerrienpro@gmail.com>
2022-10-16 18:16:00 +0200

    4 niveaux faits

--- src/lib/Bed.svelte
+++ src/lib/Bed.svelte
@@ -1,6 +1,6 @@
 <script>
     export let bed;
-    console.log(bed);
+    // console.log(bed);
 </script>
 
 <div>Type de bois: {bed.bois}</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,178 +1,294 @@
 <script>
-    import { supabase } from "$lib/supabaseClient.js";
     import Bed from "../lib/Bed.svelte";
     import Dressing from "../lib/Dressing.svelte";
     import Tv from "../lib/Tv.svelte";
 
-    async function fetchAllProperties() {
-        const { data, error } = await supabase.from("property").select(`
-            agencement->floors
-        `);
+    let floorIndex = null;
+    let roomIndex = null;
+    let furnitureIndex = null;
 
-        if (error) throw new Error(error.message);
-        return data;
+    let data = [
+        {
+            name: "Maison 1",
+            floors: [
+                {
+                    name: "Etage n°1",
+                    rooms: [
+                        {
+                            name: "Chambre n°1",
+                            surface: 10,
+                            furnitures: [
+                                {
+                                    bois: "Chêne",
+                                    name: "Lit n°1",
+                                    type: "bed",
+                                },
+                                {
+                                    name: "Dressing n°1",
+                                    type: "dressing",
+                                    surface: 20,
+                                },
+                                {
+                                    name: "Dressing n°2",
+                                    type: "dressing",
+                                    surface: 10,
+                                },
+                                {
+                                    name: "TV n°1",
+                                    type: "tv",
+                                    definition: "4K",
+                                    dimensions: [20, 30],
+                                },
+                            ],
+                        },
+                        {
+                            name: "Chambre n°2",
+                            surface: 20,
+                            furnitures: [
+                                {
+                                    bois: "Chêne",
+                                    name: "Lit n°1",
+                                    type: "bed",
+                                },
+                                {
+                                    name: "Dressing n°1",
+                                    type: "dressing",
+                                    surface: 30,
+                                },
+                                {
+                                    name: "Dressing n°2",
+                                    type: "dressing",
+                                    surface: 40,
+                                },
+                                {
+                                    name: "TV n°1",
+                                    type: "tv",
+                                    definition: "4K",
+                                    dimensions: [30, 40],
+                                },
+                            ],
+                        },
+                    ],
+                    surface: 100,
+                },
+                {
+                    name: "Etage n°2",
+                    rooms: [
+                        {
+                            name: "Chambre n°1",
+                            surface: 10,
+                            furnitures: [
+                                {
+                                    bois: "Chêne",
+                                    name: "Lit n°1",
+                                    type: "bed",
+                                },
+                                {
+                                    name: "Dressing n°1",
+                                    type: "dressing",
+                                    surface: 20,
+                                },
+                                {
+                                    name: "Dressing n°2",
+                                    type: "dressing",
+                                    surface: 10,
+                                },
+                                {
+                                    name: "TV n°1",
+                                    type: "tv",
+                                    definition: "4K",
+                                    dimensions: [20, 30],
+                                },
+                            ],
+                        },
+                    ],
+                    surface: 200,
+                },
+            ],
+        },
+    ];
+
+    function setFloorIndex(index) {
+        floorIndex = index;
+        console.log(floorIndex);
     }
 
-    function handleFloorCtnClick(e) {
-        console.log(e);
+    function setRoomIndex(index) {
+        roomIndex = index;
+        console.log(roomIndex);
+    }
+
+    function setFurnitureIndex(index) {
+        furnitureIndex = index;
+        console.log(furnitureIndex);
+    }
+
+    function addItem(item, b, c) {
+        if (item === "floor") {
+            const newItemIndex = data[0].floors.length + 1;
+            data[0].floors = [
+                ...data[0].floors,
+                { name: "Etage n°" + newItemIndex, rooms: [] },
+            ];
+        } else if (item === "room") {
+            if (floorIndex !== b) {
+                return;
+            }
+            const newItemIndex = data[0].floors[floorIndex].rooms.length + 1;
+            data[0].floors[floorIndex].rooms = [
+                ...data[0].floors[floorIndex].rooms,
+                { name: "Pièce n°" + newItemIndex, furnitures: [] },
+            ];
+        } else if (item === "furniture") {
+            if (floorIndex !== b || roomIndex !== c) {
+                return;
+            }
+            const newItemIndex =
+                data[0].floors[floorIndex].rooms[roomIndex].furnitures.length +
+                1;
+            data[0].floors[floorIndex].rooms[roomIndex].furnitures = [
+                ...data[0].floors[floorIndex].rooms[roomIndex].furnitures,
+                { name: "Meuble n°" + newItemIndex },
+            ];
+        }
     }
 </script>
 
-<h1>Edition Maison</h1>
-{#await fetchAllProperties()}
-    <p>Chargement Maison...</p>
-{:then data}
-    {#each data as property}
-        <div class="house-ctn show">
-            {#each property.floors as floor, a}
+{#each data as property, a}
+    <div class="ctn">
+        {#each property.floors as floor, b}
+            <button
+                class="btn"
+                on:click|self={() => setFloorIndex(b)}
+                on:click|self={() => setRoomIndex(null)}
+                on:click|self={() => setFurnitureIndex(null)}
+            >
+                <div>{floor.name}</div>
                 <div
-                    class="house-btn"
-                    data-floor={a}
-                    on:keypress={handleFloorCtnClick}
+                    class="ctn"
+                    class:selected={floorIndex === b && roomIndex === null}
                 >
-                    <div class="house-btn-item">{floor.name}</div>
-                    <div class="floor-ctn hide" data-floor={a}>
-                        {#each floor.rooms as room}
-                            <div class="floor-btn">
-                                <div class="floor-item">{room.name}</div>
-                                <div class="room-ctn hide" data-floor={a}>
-                                    {#each room.furnitures as furniture}
-                                        <div class="room-btn">
-                                            <div class="room-item">
-                                                <div>{furniture.name}</div>
-                                                {#if furniture.type === "bed"}
-                                                    <Bed bed={furniture} />
-                                                {:else if furniture.type === "dressing"}
-                                                    <Dressing
-                                                        dressing={furniture}
-                                                    />
-                                                {:else if furniture.type === "tv"}
-                                                    <Tv tv={furniture} />
-                                                {/if}
-                                            </div>
+                    {#each floor.rooms as room, c}
+                        <button
+                            class="btn"
+                            on:click|self={() => setFloorIndex(b)}
+                            on:click|self={() => setRoomIndex(c)}
+                            on:click|self={() => setFurnitureIndex(null)}
+                        >
+                            <div>{room.name}</div>
+                            <div
+                                class="ctn"
+                                class:selected={floorIndex === b &&
+                                    roomIndex === c &&
+                                    furnitureIndex === null}
+                            >
+                                {#each room.furnitures as furniture, d}
+                                    <button
+                                        class="btn"
+                                        on:click|self={() => setFloorIndex(b)}
+                                        on:click|self={() => setRoomIndex(c)}
+                                        on:click|self={() =>
+                                            setFurnitureIndex(d)}
+                                    >
+                                        <div>{furniture.name}</div>
+                                        <div
+                                            class="ctn"
+                                            class:selected={floorIndex === b &&
+                                                roomIndex === c &&
+                                                furnitureIndex === d}
+                                        >
+                                            {#if furniture.type === "bed"}
+                                                <Bed bed={furniture} />
+                                            {:else if furniture.type === "dressing"}
+                                                <Dressing
+                                                    dressing={furniture}
+                                                />
+                                            {:else if furniture.type === "tv"}
+                                                <Tv tv={furniture} />
+                                            {/if}
                                         </div>
-                                    {/each}
-                                </div>
+                                    </button>
+                                {/each}
+                                <button
+                                    class="add-btn"
+                                    on:click={() => addItem("furniture", b, c)}
+                                    >Ajouter un meuble</button
+                                >
                             </div>
-                        {/each}
-                    </div>
+                        </button>
+                    {/each}
+                    <button class="add-btn" on:click={() => addItem("room", b)}
+                        >Ajouter une pièce</button
+                    >
                 </div>
-            {/each}
-        </div>
-    {/each}
-{:catch error}
-    <p>Something went wrong while fetching the data:</p>
-    <pre>{error}</pre>
-{/await}
+            </button>
+        {/each}
+        <button class="add-btn" on:click={() => addItem("floor")}
+            >Ajouter un étage</button
+        >
+    </div>
+{/each}
+
+<div class="ctn room-list-ctn">
+    <ul>
+        <li><button>Chambre</button></li>
+        <li><button>Cuisine</button></li>
+        <li><button>Salle de bain</button></li>
+    </ul>
+</div>
 
 <style>
-    .show {
-        position: fixed;
-        top: 100px;
-        right: 50%;
+    .selected {
+        background-color: #ff3e00 !important;
+    }
+
+    .visible {
+        visibility: visible;
+    }
+    .hidden {
+        visibility: hidden;
+    }
+
+    .ctn {
+        padding: 15px;
+        border-radius: 5px;
+        background: lightgray;
         width: 90%;
-        max-width: 900px;
-        transform: translateX(50%);
     }
 
-    .hide {
-        position: fixed;
-        left: -1000px;
+    .room-list-ctn {
+        margin-top: 20px;
     }
 
-    /* House Level */
-
-    .house-ctn {
-        padding: 15px;
-        border-radius: 5px;
-        background: lightgray;
-    }
-
-    .house-btn {
-        display: flex;
-        /* display: none; */
-        padding: 15px;
-        border-radius: 5px;
-        background-color: #fff;
-        margin-bottom: 10px;
+    .add-btn {
+        display: block;
+        background-color: #0079f2;
+        color: #fff;
+        border-radius: 10px;
+        padding: 12px 12px;
         cursor: pointer;
-        outline: 1px solid transparent;
-        transition: all ease-in-out 200ms;
+    }
+    .add-btn:active {
+        background-color: #0066cc;
     }
 
-    .house-btn:hover {
+    .btn {
+        display: flex;
+        width: 100%;
+        border: none;
+        padding: 20px;
+        margin-bottom: 10px;
+        border-radius: 10px;
+        cursor: pointer;
+        background-color: #fff;
+    }
+
+    .btn:hover {
         outline: 1px solid #000;
     }
 
-    .house-btn:last-child {
-        margin-bottom: 0;
-    }
-
-    .house-btn-item {
-        margin-right: 20px;
-    }
-
-    /* Floor Level */
-
-    .floor-ctn {
-        padding: 15px;
-        border-radius: 5px;
-        background: lightgray;
-    }
-
-    .floor-btn {
-        display: flex;
-        /* display: block; */
-        padding: 15px;
-        border-radius: 5px;
-        background-color: #fff;
-        margin-bottom: 10px;
-        cursor: pointer;
-        outline: 1px solid transparent;
-        transition: all ease-in-out 200ms;
-    }
-
-    .floor-btn:hover {
-        outline: 1px solid #000;
-    }
-
-    .floor-btn:last-child {
-        margin-bottom: 0;
-    }
-
-    .floor-btn-item {
-        margin-right: 20px;
-    }
-
-    /* Room Level */
-
-    .room-ctn {
-        padding: 15px;
-        border-radius: 5px;
-        background: lightgray;
-    }
-
-    .room-btn {
-        display: flex;
-        /* display: block; */
-        padding: 15px;
-        border-radius: 5px;
-        background-color: #fff;
-        margin-bottom: 10px;
-        cursor: pointer;
-        outline: 1px solid transparent;
-        transition: all ease-in-out 200ms;
-    }
-
-    .room-btn:hover {
-        outline: 1px solid #000;
-    }
-
-    .room-btn:last-child {
-        margin-bottom: 0;
-    }
-
-    .room-btn-item {
-        margin-right: 20px;
+    ul {
+        list-style-type: none;
+        padding: 0;
     }
 </style>
